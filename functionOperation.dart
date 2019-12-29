c7928d5b3cbcad22b8d2c7ecb86018b49f6ce705
oethan <50686634+oethan@users.noreply.github.com>
2019-12-29 07:59:03 +0000

    Update functionOperation.dart

--- functionOperation.dart
+++ functionOperation.dart
@@ -1,11 +1,11 @@
 void main() {
-  add(1.5, 1);
-  subtract(3.1, 5);
-  multiply(2, 1.3);
-  divide(12, 5);
+  print(add(1.5, 1));
+  print(subtract(3.1, 5));
+  print(multiply(2, 1.3));
+  print(divide(12, 5));
  }
  
- void add (double x, double y) => print ($x + $y = ${x+y});
- void subtract (double x, double y) => print ($x - $y = ${x-y});
- void multiply (double x, double y) => print ($x * $y = ${x*y});
- void divide (double x, double y) => print ($x / $y = ${x/y});
+ double add (double x, double y) => x+y;
+ double subtract (double x, double y) => x-y;
+ double multiply (double x, double y) => x*y;
+ double divide (double x, double y) => x/y;
